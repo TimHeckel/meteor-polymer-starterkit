eb7ae671a76b897ecf82e42b7b495cf254742fd8
Tim Heckel <tim@slatebox.com>
2015-03-25 08:36:11 -0500

    Updating

--- client/views/layout/dashboard/dashboardLayout.js
+++ client/views/layout/dashboard/dashboardLayout.js
@@ -4,10 +4,25 @@ Template.dashboardLayout.helpers({
 	  		return 'right-to-left';
 	  	}
   	}
+  	, theme: function() {
+  		return Themes.find();
+  	}
 });
 
 Template.dashboardLayout.events({
 	"click [data-action=toggle-drawer]": function(e) {
 		$("[data-drawer-panel]")[0].togglePanel();
 	}
+});
+
+Template.dashboardLayout.onRendered(function() {
+	var ddl = document.querySelector("#ddlTheme");
+	//firing twice is normal for now
+	//http://stackoverflow.com/questions/24474020/how-does-core-selectors-core-select-event-work
+	ddl.addEventListener('core-select', function(e) {
+		console.log("cahnged theme...", arguments);
+		if (e.detail.isSelected) {
+			$("head").find("#currentTheme")[0].href = "themes/" + $(e.detail.item).attr("data-src");
+		}
+	});
 });
--- server/startup.js
+++ server/startup.js
@@ -6,4 +6,22 @@ Meteor.startup(function() {
 	if (ReactiveText.find({ _id: "reactive" }).count() === 0) {
 		ReactiveText.insert({ _id: "reactive", text: "" });
 	}
+	
+	if (Themes.find().count() === 0) {
+
+		var _themes = [
+			{ src: "candy.css", name: "Candy" }
+			, { src: "classical.css", name: "Classical" }
+			, { src: "darkside.css", name: "Darkside" }
+			, { src: "denim.css", name: "Denim" }
+			, { src: "goldengoose.css", name: "Golden Goose" }
+			, { src: "ice.css", name: "Ice Ice Baby" }
+			, { src: "melon.css", name: "Melon" }
+		];
+		
+		_.each(_themes, function(t) {
+			Themes.insert(t);
+		});
+
+	}
 });
